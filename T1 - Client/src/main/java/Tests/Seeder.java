ea9c527115ad508c2432e0a23c761fc0fb7bfe70
Augusto-Rustick <a.rustick@outlook.com>
2023-03-22 20:58:48 -0300

    feat: criação de seeder para o banco de dados

---
+++ T1 - Client/src/main/java/Tests/Seeder.java
@@ -0,0 +1,44 @@
+package Tests;
+
+import Socket.RequestService;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Seeder {
+    public static void main(String[] args) {
+        try {
+            //instancia request
+            RequestService req = new RequestService();
+
+            //cria uma instancia de departamento
+            String insertDepartamento = "departamento;Insert;departamentadasso;caixas;15";
+            req.setRequest(insertDepartamento);
+            req.execute();
+            req.resetRequestService();
+
+            //Cria alguns instancias de funcionario
+            String insertFuncionarioStart = "funcionario;Insert;1111111111";
+            String insertFuncionarioEnd = ";Augusto;ruaruaruaaaa;asgfsaf;0;1;";
+            for (int c = 1; c < 5; c++) {
+                String insertFuncionario = insertFuncionarioStart + c + insertFuncionarioEnd;
+                req.setRequest(insertFuncionario);
+                req.execute();
+                req.resetRequestService();
+            }
+            //Cria alguns instancias de transportador
+            String insertTransportadorStart = "transportador;INSERT;1111111111";
+            String insertTransportadorEnd = ";jonaslima;olimpia;479974815021;120;1;";
+            for (int c = 5; c < 9; c++) {
+                String insertTransportador = insertTransportadorStart + c + insertTransportadorEnd;
+                req.setRequest(insertTransportador);
+                req.execute();
+                req.resetRequestService();
+            }
+
+        } catch (Exception ex) {
+            Logger.getLogger(executeRequest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
--- T1 - Client/src/main/java/Tests/executeRequest.java
+++ T1 - Client/src/main/java/Tests/executeRequest.java
@@ -2,7 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Socket;
+package Tests;
+
+import Socket.RequestService;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
